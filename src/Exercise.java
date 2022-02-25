71ae082a2e982fd152520e6db8dd80139de07f05
Zouthen <mich5n69@stud.kea.dk>
2022-02-25 09:30:24 +0100

    Added askAboutAge

--- src/Exercise.java
+++ src/Exercise.java
@@ -2,10 +2,13 @@ public class Exercise {
     public static void main(String[] args) {
         System.out.println("It Works!");
 
-        new Exercise().Greet();
+        new Exercise().greet();
     }
-    void Greet(){
+    void greet(){
         System.out.println("Enter name");
         System.out.println("HI someone!");
+    }
+    void askAboutAge(){
+        System.out.println("Enter age");
     }
 }

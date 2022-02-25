7baeb393e9b5ff8f0f0e87a8d383f40342685ae5
Zouthen <mich5n69@stud.kea.dk>
2022-02-25 09:27:46 +0100

    Added greet

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,5 +1,11 @@
 public class Exercise {
     public static void main(String[] args) {
         System.out.println("It Works!");
+
+        new Exercise().Greet();
+    }
+    void Greet(){
+        System.out.println("Enter name");
+        System.out.println("HI someone!");
     }
 }
